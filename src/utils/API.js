e8c8a4457d54e45633edea75ceafcd6ed17c4f2a
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-09-17 00:25:32 -0400

    added vite env

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,15 +2,21 @@ import axios from "axios";
 
 export const coordinateSearch = (query) =>
   axios.get(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${query}&appid=021e75b0e3380e236b4ff6031ae2dde4`
+    `http://api.openweathermap.org/geo/1.0/direct?q=${query}&appid=${
+      import.meta.env.VITE_API_KEY
+    }`
   );
 
 export const getWeather = (lat, lon) =>
   axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=021e75b0e3380e236b4ff6031ae2dde4&units=imperial`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${
+      import.meta.env.VITE_API_KEY
+    }`
   );
 
 export const getFiveDayWeather = (lat, lon) =>
   axios.get(
-    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=021e75b0e3380e236b4ff6031ae2dde4&units=imperial`
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${
+      import.meta.env.VITE_API_KEY
+    }`
   );
