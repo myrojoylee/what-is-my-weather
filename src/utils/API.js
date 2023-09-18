3b79c0d945206e99bad7f25606c530b4c1eda679
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-09-18 00:32:20 -0400

    temp now reads in F instead of K

--- src/utils/API.js
+++ src/utils/API.js
@@ -11,12 +11,12 @@ export const getWeather = (lat, lon) =>
   axios.get(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${
       import.meta.env.VITE_API_KEY
-    }`
+    }&units=Imperial`
   );
 
 export const getFiveDayWeather = (lat, lon) =>
   axios.get(
     `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${
       import.meta.env.VITE_API_KEY
-    }`
+    }&units=Imperial`
   );
